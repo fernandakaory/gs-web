9a661e16f3f9cf7f991823b07976f05eeda27e69
fernandakaory <rm551104@fiap.com.br>
2023-11-15 12:20:40 -0300

    adicionando textos

--- src/routes/Principal.jsx
+++ src/routes/Principal.jsx
@@ -1,9 +1,46 @@
 import "../assets/styles/Cabecalho.css"
+import agendamento from '../assets/img/agendamento.jpg'
 
 export default function Cabecalho() {
   return (
     <>
-        
+        <section>
+            <img src={agendamento} alt="Imagem principal de agendamento" />
+            <div>
+                <h2>PONTO +</h2>
+                <p>Aplicativo simples e intuitivo que busca facilitar a processo de solicitação de exames, simplificando a comunicação entre os usuários e os locais de exame, tanto conveniados quanto clínicas particulares.</p>
+            </div>
+        </section>
+        <section>
+            <div>
+                <h3>O QUE É A SOLUÇÃO</h3>
+                <p>Nossa solução é um aplicativo que será focado em facilitar para o usuário a solicitação de exames, simplificando a comunicação entre o convênio e locais de exame, tanto conveniados como clínicas particulares. O objetivo desta é garantir que os usuários tenham acesso facilitado aos procedimentos médicos necessários para uma vida mais saudável. 
+                </p>
+            </div>
+            <div>
+                <h3>O QUE ELA FARÁ</h3>
+                <p>Nossa solução vai permitir que o usuário, a partir de uma solicitação médica, consiga de forma simples e rápida agendar exames com nossas clínicas parceiras, de acordo com a data, horário e local desejados. A aplicação, vinculada a um plano de saúde, possibilitará que os usuário e profissionais visualizem seus resultados, através do uso de sensores e Internet das Coisas, para garantir mais automação. </p>
+            </div>
+            <div>
+                <h3>COMO FUNCIONARÁ</h3>
+                <p>Ao solicitar o agendamento, o usuário terá acesso aos locais disponíveis em um mapa, aos horários e datas oferecidos e o preço da consulta (caso seja fora do convênio). Os resultados dos exames estarão disponíveis para consulta dos usuários e dos médicos. Para garantir o uso da tecnologia para automação, caso o paciente precise ficar em observação ou tratamento contínuo, a partir de sensores disponibilizados, informações médicas como temperatura/febre e sinais de movimento estarão disponíveis remotamente no aplicativo, através de IOT.</p>
+            </div>
+        </section>
+        <section>
+            <h3>VANTAGENS</h3>
+        <div>
+                <h3>ECONOMIA DE TEMPO</h3>
+                <p>Com nosso aplicativo, o usuário pode economizar tempo, uma vez que não precisa fazer diversas ligações e pesquisas por clínicas para poder marcar um agendamento. Todas as informações e opções disponíveis estão centralizadas e unificadas na Ponto +.</p>
+            </div>
+            <div>
+                <h3>TUTORIAL EM VÍDEO</h3>
+                <p>Com o intuito de promover maior acessibilidade, principalmente a idosos, será disponibilizado um vídeo de tutorial de como utilizar nossa aplicação. Este estará disponível a quaaluqer momento para quem precisar de ajuda.</p>
+            </div>
+            <div>
+                <h3>COMUNICAÇÃO FACILITADA</h3>
+                <p>Além de facilitar o agendamento, buscamos facilitar a comunicação entre as clínicas e os usuários. Nosso aplicativo contará com chats para que os futuros pacientes e os locais de exame possam conversar e esclarecer dúvidas.</p>
+            </div>
+        </section>
     </>
   );
 }
